e71f94e612ab046147dd7c4b0b02b32c9868eda1
Markos Kapes <mkapes@gmail.com>
2011-10-23 23:50:40 -0400

    new file:   infoshopkeeper/.DS_Store 	deleted:    infoshopkeeper/NotesTemplate.tmpl 	new file:   infoshopkeeper/inventoryserver/transaction.py 	modified:   infoshopkeeper/inventoryserver/AddToInventoryTemplate.py 	modified:   infoshopkeeper/inventoryserver/AuthorEditTemplate.py 	modified:   infoshopkeeper/inventoryserver/BaseSearchTemplate.py 	modified:   infoshopkeeper/inventoryserver/BookEditTemplate.py 	modified:   infoshopkeeper/inventoryserver/CartTemplate.py 	modified:   infoshopkeeper/inventoryserver/CartTemplate2.py 	modified:   infoshopkeeper/inventoryserver/CategoryEditTemplate.py 	modified:   infoshopkeeper/inventoryserver/CheckoutTemplate.py 	modified:   infoshopkeeper/inventoryserver/ChooseItemTemplate.py 	modified:   infoshopkeeper/inventoryserver/IndexTemplate.py 	modified:   infoshopkeeper/inventoryserver/KindEditTemplate.py 	modified:   infoshopkeeper/inventoryserver/KindListTemplate.py 	modified:   infoshopkeeper/inventoryserver/LocationEditTemplate.py 	modified:   infoshopkeeper/inventoryserver/LocationListTemplate.py 	modified:   infoshopkeeper/inventoryserver/NotesTemplate.py 	modified:   infoshopkeeper/inventoryserver/ReportListTemplate.py 	modified:   infoshopkeeper/inventoryserver/ReportTemplate.py 	modified:   infoshopkeeper/inventoryserver/SearchTemplate.py 	modified:   infoshopkeeper/inventoryserver/Skeleton.py 	modified:   infoshopkeeper/inventoryserver/TestTemplate.py 	modified:   infoshopkeeper/inventoryserver/TitleEditTemplate.py 	modified:   infoshopkeeper/inventoryserver/TitleListTemplate.py 	modified:   infoshopkeeper/inventoryserver/TransactionsTemplate.py 	modified:   infoshopkeeper/inventoryserver/test.py

---
+++ Alarm Clock/Alarm ClockTests/Alarm_ClockTests.m
@@ -0,0 +1,32 @@
+//
+//  Alarm_ClockTests.m
+//  Alarm ClockTests
+//
+//  Created by Markos Kapes on 9/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Alarm_ClockTests.h"
+
+@implementation Alarm_ClockTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Alarm ClockTests");
+}
+
+@end
