c30294f38e8ed14c9c85be4f20a4dbc16be39a7e
Markos Kapes <mkapes@gmail.com>
2011-10-24 14:16:57 -0400

    pruned tracked files (.py files of templates, etc)

--- infoshopkeeper/objects/transaction.py
+++ infoshopkeeper/objects/transaction.py
@@ -12,28 +12,32 @@ import etc
 #_connection = db.SQLObjconnect()
 
 class Transaction(SQLObjectWithFormGlue):
-	_table = "transactionLog"
-	_connection = db.conn() 
-	action=StringCol()
-	amount=FloatCol()
-	date=DateTimeCol(default=now)
-	schedule = StringCol()            
-	info =StringCol()
-	owner =StringCol()
-	cashier =StringCol() 
+    _connection = db.conn() 
 
-	def object_to_form(self):
-		self.extracolumns()
-		return SQLObjectWithFormGlue.object_to_form(self)
+    class sqlmeta:
+        from_database=True
+        table = "transactionLog"
+    
+    action=StringCol()
+    amount=FloatCol()
+    date=DateTimeCol(default=now)
+    schedule = StringCol()            
+    info =StringCol()
+    owner =StringCol()
+    cashier =StringCol() 
 
-	def extracolumns(self):
-		pass
+    def object_to_form(self):
+        self.extracolumns()
+        return SQLObjectWithFormGlue.object_to_form(self)
 
-	def void(self):
-		pass
+    def extracolumns(self):
+        pass
 
-	def get_info(self):
-		if 'tostring' in dir(self.info):
-			return self.info.tostring()
-		else:
-			return self.info
+    def void(self):
+        pass
+
+    def get_info(self):
+        if 'tostring' in dir(self.info):
+            return self.info.tostring()
+        else:
+            return self.info
