95462a1bed1facb01a6c650d430ad3b898361908
Markos Kapes <mkapes@gmail.com>
2016-03-01 14:05:48 -0500

    clean up addioton of icons

---
+++ infoshopkeeper/NoIP-renew-host-monthly.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from robobrowser import RoboBrowser
+
+browser=RoboBrowser()
+browser.open("https://www.noip.com/members/dns/host.php?host_id=48545720")
+browser.get_form(id='clogs')
+form=browser.get_form(id='clogs')
+form['username']='woodenshoebooks'
+form['password']='woodenshoe'
+browser.submit_form(form)
+browser.get_forms()
+browser.submit_form(browser.get_forms()[0])
+
