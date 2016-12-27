c2022b157c475429ad7e09a8eba21c1c66488970
Markos Kapes <mkapes@gmail.com>
2016-12-27 14:42:20 -0500

    quick fix on making amazon not search for internal isbns.

--- infoshopkeeper/tools/inventory.py
+++ infoshopkeeper/tools/inventory.py
@@ -123,10 +123,15 @@ def lookup_by_isbn(number, forceUpdate=False):
             if len(isbn)==12:
                 idType='UPC'
             elif len(isbn)==13:
-                if isbn.startswith('978') or isbn.startswith('979'):
+                #if we are using an internal isbn
+                if isbn.startswith('199'):
+                    return []
+                #otherwise search on amazon.
+                elif isbn.startswith('978') or isbn.startswith('979'):
                     idType='ISBN'
                 else:
                     idType='EAN'
+        
             
             print>>sys.stderr, "idtype ",  idType
             try:
