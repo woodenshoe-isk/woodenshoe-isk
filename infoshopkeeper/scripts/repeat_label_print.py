9b0da0cfa69506f0257a5a8c45b7574f01977b89
Markos Kapes <mkapes@gmail.com>
2017-01-06 17:02:57 -0500

    AddToInventory changes, add repeat_label_print
    1. made AddToInventory check "print label" box when isbn is does not
    have a proper exended price code, or if ourprice and listprice aren't
    the same.
    2. added a script to enable printing labels of multiple books by isbn.

---
+++ infoshopkeeper/scripts/repeat_label_print.py
@@ -0,0 +1,62 @@
+import re
+import gnureadline as readline
+from sqlobject import sqlbuilder
+
+from objects.book import Book
+from objects.title import Title
+from objects.location import Location
+
+from printing import barcodeLabel
+
+def rlinput(prompt, prefill=''):
+    print prefill
+    def hook(prefill):
+        print prefill
+        readline.insert_text(prefill)
+
+    readline.set_startup_hook(hook)
+    try:
+        return raw_input(prompt)
+    finally:
+        readline.set_startup_hook()
+
+should_quit = False
+while should_quit != True:
+    isbn  = raw_input('isbn or title >> ')
+    if isbn.lower().strip() == 'quit' or isbn.lower().strip()=='q':
+        should_quit=True
+        continue
+    if re.match('^[0-9]{13}$|^[0-9]{18}$', isbn):
+        try:
+            isbn, price = isbn[0:13], float(isbn[13, -1])
+        except:
+            isbn, price = isbn[0:13], 0.00
+        titles = Title.selectBy(isbn=isbn)
+        if list(titles):
+            for t1 in titles:
+                ourprice = rlinput("price >> ", prefill=price)                                                             
+                books = Book.selectBy(titleID=t1.id, ourprice=float(ourprice), status='STOCK')
+                if list(books):
+                    ourprice = books[0].ourprice
+                    book = books[0]
+                    break
+            if not book:        
+                listprice = Book.selectBy(titleID=t1.id).max(Book.q.listprice) 
+                book = Book(title=t1[0], status='STOCK', location=1, owner='woodenshoe', listprice=float(listprice), ourprice=float(book.ourprice), consignmentStatus='', distributor='', notes='')
+            barcodeLabel.print_barcode_label(isbn=isbn, booktitle=t1.booktitle, ourprice=book.ourprice, listprice=listprice, num_copies=1)
+        else:
+            continue
+    else:
+        titles = Title.select(sqlbuilder.RLIKE(Title.q.booktitle, isbn))
+        if list(titles):
+            for n, t1 in enumerate(titles):
+                print "%d. %s   %s" % (n, t1.booktitle, t1.type)
+            n_book = int(raw_input("select book >> "))
+            t1 = titles[n_book]
+            price = raw_input('price >> ')
+            listprice = Book.selectBy(titleID=t1.id).max(Book.q.listprice)
+            book = Book(title=t1, status='STOCK', location=1, owner='woodenshoe', listprice=float(listprice), ourprice=float(price), consignmentStatus='', distributor='', notes='')
+            barcodeLabel.print_barcode_label(isbn=t1.isbn, booktitle=t1.booktitle, ourprice=price, listprice=listprice, num_copies=1)
+        else:   
+             continue
+
